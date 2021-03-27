66e945ddb719d38485fe75525a228f2a95acf307
ehom <ehom@acm.org>
2021-03-27 01:18:14 -0700

    get headlines of specified countries

---
+++ fetch-headlines.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+from urllib.request import urlopen
+import json
+import pprint
+
+
+def fetch(url):
+    response = urlopen(url)
+    data = json.loads(response.read())
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(data)
+    return data
+
+
+def saveToFile(data, fileName):
+    with open(fileName, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+def main(args):
+    base_url, base_filename, countries = args
+
+    if base_url: print("base_url:", base_url)
+    if base_filename: print("base_filename:", base_filename)
+    if countries: print("countries:", countries)
+
+    for code in countries.split(','):
+      url = "{}&country={}".format(base_url, code)
+      filename = "{}-{}".format(code, base_filename)
+
+      print("url:", url)
+      print("filename:", filename)
+
+      data = fetch(url)
+      saveToFile(data, filename)
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 4):
+        print("Usage: {0} URL output_filename countries".format(sys.argv[0]))
+        os._exit(1)
+
+    main(sys.argv[1:])
