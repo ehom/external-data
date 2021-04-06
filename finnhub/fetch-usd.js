2d0621c67d3b9d2b078689469c5493e5a2fb41fb
ehom <ehom@acm.org>
2021-04-05 18:39:34 -0700

    add tool to fetch USD exchange rates

---
+++ finnhub/fetch-usd.js
@@ -0,0 +1,19 @@
+const finnhub = require('finnhub');
+const fs = require('fs');
+
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = process.env.FINNHUB_API_KEY;
+
+const finnhubClient = new finnhub.DefaultApi()
+
+// Forex rates
+finnhubClient.forexRates({"base": "USD"}, (error, data, response) => {
+    data.timeStamp = new Date(Date.now()); // ISO
+    console.log(data);
+    saveJsonTo('usd.json', data);
+});
+
+const saveJsonTo = (filename, data) => {
+  const text = JSON.stringify(data, null, 2);
+  fs.writeFileSync(filename, text);
+};
