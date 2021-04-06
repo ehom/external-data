08ad17a720b46c50dd03678817f4ce8149a672a3
ehom <ehom@users.noreply.github.com>
2021-04-05 19:20:07 -0700

    remove debug statements

--- finnhub/fetch-usd.js
+++ finnhub/fetch-usd.js
@@ -6,14 +6,11 @@ const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 const [secret] = process.argv.slice(2);
 api_key.apiKey = secret;
 
-console.log(secret);
-
 const finnhubClient = new finnhub.DefaultApi()
 
 // Forex rates
 finnhubClient.forexRates({"base": "USD"}, (error, data, response) => {
     data.timeStamp = new Date(Date.now()); // ISO
-    console.log(data);
     saveJsonTo('usd.json', data);
 });
 
