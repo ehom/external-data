bf0681caf3313acc0f13767a4de9a2b386ddb385
ehom <ehom@users.noreply.github.com>
2022-02-17 19:06:15 -0800

    update package.json and handle failure in code
    use latest version of finnhub.
    
    handle case where data returned from api is null

--- finnhub/fetch-usd.js
+++ finnhub/fetch-usd.js
@@ -2,19 +2,20 @@ const finnhub = require('finnhub');
 const fs = require('fs');
 
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+
 // TODO: error handling
 const [secret] = process.argv.slice(2);
-api_key.apiKey = secret;
-
 const finnhubClient = new finnhub.DefaultApi()
 
 // Forex rates
 finnhubClient.forexRates({"base": "USD"}, (error, data, response) => {
+  if (data) {
     console.debug("data object:", data);
-    if ('timeStamp' in data) {
+    if (data['timeStamp']) {
         data['timeStamp'] = new Date(Date.now()); // ISO
     }
     saveJsonTo('usd.json', data);
+  }
 });
 
 const saveJsonTo = (filename, data) => {
